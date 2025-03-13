50280fb4cf2fd3ee4823048503f6e7482ec96b93
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2025-03-13 20:03:22 +0000

    - Initial draft.

---
+++ bin/gen-timetable.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+
+use v5.38;
+use Time::Piece;
+
+my $year_month = localtime->strftime('%Y-%m');
+my ($year, $month) = split /\-/, $year_month, 2;
+
+our $IN    = $ARGV[0];
+die "Usage: $0 <input_folder> <output_folder> <year> <month>\nERROR: Missing input foler.\n"
+    unless (defined $IN);
+
+our $OUT   = $ARGV[1];
+die "Usage: $0 <input_folder> <output_folder> <year> <month>\nERROR: Missing output folder.\n"
+    unless (defined $OUT);
+
+our $YEAR  = $ARGV[2] || $year;
+our $MONTH = $ARGV[3] || $month;
+
+die "Usage: $0 <input_folder> <output_folder> <year> <month>\nERROR: Invalid year $YEAR.\n"
+    unless ($YEAR > 0);
+
+die "Usage: $0 <input_folder> <output_folder> <year> <month>\nERROR: Invalid month $MONTH.\n"
+    unless ($MONTH >= 1 && $MONTH <= 12);
+
+my $IN_FILE  = sprintf("%s/%04d-%02d.txt", $IN, $YEAR, $MONTH);
+my $OUT_FILE = sprintf("%s/%04d-%02d.txt", $OUT, $YEAR, $MONTH);
+
+open(my $out, '>', $OUT_FILE)
+    or die "Could not open file '$OUT_FILE' $!";
+open(my $in,  '<', $IN_FILE)
+    or die "Could not open file '$IN_FILE' $!";
+
+while (my $line = <$in>) {
+    chomp($line);
+
+    $line =~ s/ +/ /g;
+    $line =~ s/\t/ /g;
+    my @columns = split /\s/, $line;
+
+    my $day = sprintf("%02d", $columns[0]);
+    my @selected_times = ($columns[1], $columns[4], $columns[6], $columns[9], $columns[11]);
+
+    my $i = 0;
+    foreach my $time (@selected_times) {
+        my ($hour, $minute) = split /:/, $time;
+        $hour = sprintf("%02d", $hour);
+        $minute = sprintf("%02d", $minute);
+        $time = ($i == 0)? "$hour:$minute|F" : "$hour:$minute|R";
+        $i++;
+    }
+
+    foreach my $time (@selected_times) {
+        print $out "$year-$month-$day $time\n";
+    }
+}
+
+close $in;
+close $out;
---
+++ bin/play-athan.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/env perl
+
+use v5.38;
+use Time::Piece;
+use FFI::Platypus;
+use FFI::Platypus::Record;
+use Time::HiRes qw(usleep);
+use POSIX qw(strftime setsid);
+
+our $VERBOSE = $ENV{ATHAN_VERBOSE} || 0;
+
+my $TIME = $ARGV[0];
+die "Usage: $0 <athan_time_folder> <athan_folder> <log_folder>\nERROR: Missing athan time foler.\n"
+    unless (defined $TIME);
+my $ATHAN = $ARGV[1];
+die "Usage: $0 <athan_time_folder> <athan_folder> <log_folder>\nERROR: Missing athan foler.\n"
+    unless (defined $ATHAN);
+my $LOG = $ARGV[2] || '/tmp';
+
+my $year_month = localtime->strftime('%Y-%m');
+my ($year, $month) = split /\-/, $year_month, 2;
+
+my $TIME_FILE = sprintf("%s/%04d-%02d.txt", $TIME, $year, $month);
+my $LOG_FILE  = sprintf("%s/athan-app.log", $LOG);
+
+sub read_scheduled_times {
+    open(my $fh, '<', $TIME_FILE)
+        or die "Could not open file '$TIME_FILE' $!";
+    my %scheduled_times;
+    while (my $line = <$fh>) {
+        chomp($line);
+        my ($time, $type) = split /\|/, $line, 2;
+        $scheduled_times{$time} = $type;
+    }
+    close $fh;
+    return %scheduled_times;
+}
+
+sub log_it($message) {
+    open(my $log_fh, '>>', $LOG_FILE)
+        or die "Could not open '$LOG_FILE': $!";
+    print $log_fh $message, "\n";
+    close $log_fh;
+}
+
+sub play_athan($type) {
+
+    # Initialize FFI
+    my $ffi = FFI::Platypus->new;
+    $ffi->lib('/usr/lib/x86_64-linux-gnu/libSDL2.so');
+    $ffi->lib('/usr/lib/x86_64-linux-gnu/libSDL2_mixer.so');
+
+    # Define SDL2 and SDL2_mixer functions
+    $ffi->attach('SDL_Init' => ['int'] => 'int');
+    $ffi->attach('Mix_OpenAudio' => ['int', 'uint16', 'int', 'int'] => 'int');
+    $ffi->attach('Mix_LoadMUS' => ['string'] => 'opaque');
+    $ffi->attach('Mix_PlayMusic' => ['opaque', 'int'] => 'int');
+    $ffi->attach('Mix_PlayingMusic' => [] => 'int');
+    $ffi->attach('Mix_HaltMusic' => [] => 'void');
+    $ffi->attach('Mix_FreeMusic' => ['opaque'] => 'void');
+    $ffi->attach('Mix_CloseAudio' => [] => 'void');
+    $ffi->attach('SDL_Quit' => [] => 'void');
+    $ffi->attach('SDL_PollEvent' => ['opaque'] => 'int');
+    $ffi->attach('SDL_Delay' => ['uint32'] => 'void');
+
+    # Constants
+    use constant SDL_INIT_AUDIO => 0x00000010;
+    use constant MIX_DEFAULT_FORMAT => 32784;
+    use constant MIX_DEFAULT_CHANNELS => 2;
+    use constant MIX_DEFAULT_FREQUENCY => 44100;
+    use constant SDL_QUIT => 0x100;
+
+    # Initialize SDL and SDL_mixer
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 20480);
+
+    my $athan_file;
+    if (defined $type && ($type eq 'F')) {
+        $athan_file = sprintf("%s/fajar-athan.mp3", $ATHAN);
+    }
+    else {
+        $athan_file = sprintf("%s/regular-athan.mp3", $ATHAN);
+    }
+
+    # Load the MP3 file
+    my $music = Mix_LoadMUS($athan_file);
+    die "ERROR: Failed to load $athan_file." unless $music;
+
+    my $message = "Playing athan at " . localtime->strftime('%Y-%m-%d %H:%M') . "\n";
+    log_it($message);
+
+    # Play the athan file
+    Mix_PlayMusic($music, 0);
+
+    # Wait for the playback to finish
+    while (Mix_PlayingMusic()) {
+        usleep(50_000);
+    }
+
+    # Clean up
+    Mix_HaltMusic();
+    Mix_FreeMusic($music);
+    Mix_CloseAudio();
+    SDL_Quit();
+}
+
+## MAIN PROGRAM
+
+my %scheduled_times = read_scheduled_times();
+while (1) {
+    my $current_tp   = localtime;
+    my $current_time = $current_tp->strftime('%Y-%m-%d %I:%M');
+
+    if (exists $scheduled_times{$current_time}) {
+        play_athan($scheduled_times{$current_time});
+    }
+    else {
+        log_it("SLEEP: $current_time") if $VERBOSE;
+    }
+    sleep 10;
+}
